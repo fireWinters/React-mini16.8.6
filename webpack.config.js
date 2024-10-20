425ea470104814c1c9dd0c0fd0828a1cd415b19d
Diana Tang <1987567948@qq.com>
2024-10-20 20:26:48 +0800

    feat:Configure Webpack parameters

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [['@babel/preset-env', { targets: 'defaults' }]],
+          },
+        },
+      },
+    ],
+  },
+  optimization: {
+    minimize: false,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'React',
+      template: path.join(__dirname, 'src/index.html'), // 源模板文件
+    }),
+  ],
+};
